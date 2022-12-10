a833b8a9f6eb5424111b89147e10007409ab40ba
ClintonAloo <bclish.clinton4@example.com>
2022-12-10 02:44:55 -0800

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: zero
+ */
+int main(void)
+{
+	int a, b, z;
+
+	for (a = 48; a < 58; a++)
+	
+	{
+		for (z = 50; z < 58; z++)
+		{
+			if (z > b && b > a)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(z);
+				if (a != 55 || b != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
