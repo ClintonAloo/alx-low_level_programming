2cc0da55d1eab2a16923d4b2f06a9fac09f3b438
ClintonAloo <bclish.clinton4@example.com>
2022-12-14 09:55:41 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - shows the last didgit
+ * of a number
+ * @i: input in integer format
+ * Return: value of lst digit.
+ */
+int print_last_digit(int i)
+{
+	int m;
+
+	if (i < 0)
+	{
+		m = (i % 10) * -1;
+	}
+	else
+	{
+		m = (i % 10);
+	}
+	putchar(m + '0');
+	return (m);
+}
