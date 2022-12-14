d6e45316a15b11537f05a2444ebc74c5f55817d4
ClintonAloo <bclish.clinton4@example.com>
2022-12-14 10:31:34 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the minutes and hours of the day
+ *
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 00; m < 60; m++)
+		{
+			int h1 = (h / 10) + 48;
+			int h2 = (h % 10) + 48;
+			int m1 = (m / 10) + 48;
+			int m2 = (m % 10) + 48;
+
+			_putchar (h1);
+			_putchar (h2);
+			_putchar (':');
+			_putchar (m1);
+			_putchar (m2);
+			_putchar ('\n');
+		}
+	}
+}
+
