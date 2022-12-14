399b85add3e879886ef52227ea98bb31c24da3d3
ClintonAloo <bclish.clinton4@example.com>
2022-12-14 04:47:36 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *main - prints the alphabet in lowercase followed by a new line
+ *
+ *Return: 0 success
+ *
+ */
+void print_alphabet(void)
+{
+	char h;
+
+	for(h='a'; h<='z'; h++)
+	{
+		_putchar(h);
+	}
+	_putchar('\n');
+}	
+
