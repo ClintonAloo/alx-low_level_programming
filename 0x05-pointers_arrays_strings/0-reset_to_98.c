a420d7eec42d8e35eb0b18b2c20b8936be31e845
ClintonAloo <bclish.clinton4@example.com>
2022-12-19 08:17:17 -0800

    0.battery 98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
+
 /**
  * reset_to_98 - uses pointer to update the value it points
- * to 98
- *
+ * 		to 98
  * @n: pointer to an integer
  */
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <string.h>
@@ -12,4 +12,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 #endif /* MAIN_H */
