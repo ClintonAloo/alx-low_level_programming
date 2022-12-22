1c0e04b615e3106def12e997d0a088541c4119a5
ClintonAloo <bclish.clinton4@example.com>
2022-12-21 23:06:17 -0800

    1-concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncat - concatenates two strings
  * @dest: string 1
@@ -11,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	while (*(dest + i) != '\n')
+	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
