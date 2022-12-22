9ad18396d5283b26cb65bb76b144f72033da15e5
ClintonAloo <bclish.clinton4@example.com>
2022-12-21 23:16:33 -0800

    3-compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strncmp - compares two strings
- * @s1: string 1
+ * _strcmp - compares two strings
+ * @s1: String 1
  * @s2: string 2
  * Return: result
  */
@@ -20,6 +20,6 @@ int _strcmp(char *s1, char *s2)
 		}
 		i++;
 	}
+
 	return (0);
 }
-
