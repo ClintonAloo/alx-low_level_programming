b419707d0136ee7fb3cd27810ad4c4843f7f2ba7
ClintonAloo <bclish.clinton4@example.com>
2023-01-13 06:39:33 -0800

    The function concatenates two Strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,15 +26,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str = malloc(sizeof(char) * (s1_len + 1));
 		if (str == NULL)
-			return NULL;
+			return (NULL);
 		strcpy(str, s1);
-		return str;
+		return (str);
 	}
 	if (n > s2_len)
 		n = s2_len;
 	str = malloc(sizeof(char) * (s1_len + n + 1));
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
 	for (j = 0; j < n; j++)
@@ -43,5 +43,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 	str[i] = '\0';
-	return str;
+	return (str);
 }
