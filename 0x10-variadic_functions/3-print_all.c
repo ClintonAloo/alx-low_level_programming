43a056767d69ed1fbee6b73020c98df48d684470
ClintonAloo <bclish.clinton4@example.com>
2023-01-20 14:06:17 -0800

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,55 +1,120 @@
-i#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
 
 /**
- * print_all - prints anything according to the format string passed
- * @format: format string that specifies the types of
- *			arguments passed to the function
- * c: char
- * i: integer
- * f: float
- * s: char * (if the string is NULL, print (nil) instead
- *		any other char should be ignored
- *
- * Return: Nothing
+ * print_char - prints a char
+ * @arg: a list of arguments pointing to the character
  */
 
+void print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+	printf("%c", letter);
+}
+
+/**
+ * print_int - prints an int
+ * @arg: a list of arguments pointing to the character
+ */
+
+void print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+	printf("%c", letter);
+}
+
+/**
+ * print_int - prints an int
+ * @arg: A list of arguments pointing to the integer
+ */
+
+void print_int(va_list arg)
+{
+	int num;
+
+	num = va_arg(arg, int);
+	printf("%d", num);
+}
+
+/**
+ * print_float - prints a float
+ * @arg: a list arguments pointing to a float
+ */
+
+void print_float(va_list arg)
+{
+	float num;
+
+	num = va_arg(arg, double);
+	printf("%f", num);
+}
+
+/**
+ * print_string - prints string
+ * @arg: a list of arguments pointing to string
+ */
+
+void print_string(va_list arg)
+{
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - prints anything followed by new line
+ * @format: a string of character representing the arguments
+ * Description: any charcter is ignored if string is NULL,(nil) is printed
+ */
 void print_all(const char * const format, ...)
 {
 	va_list args;
+	int i = 0, j = 0;
+	char *separator = "";
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
 	va_start(args, format);
 
-	char c;
-	int i = 0;
-	while (format[i])
+	while (format && (*(format + i)))
 	{
-		c = format[i];
-		if (c == 'c')
+		j = 0;
+	
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
+		if (j < 4)
 		{
-			printf("%c", va_arg(args, int));
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
 		}
-		else if (c == 'i')
-		{
-			printf("%d", va_arg(args, int));
-		}
-		else if (c == 'f')
-		{
-			printf("%f", va_arg(args, double));
-		}
-		else if (c == 's')
-		{
-			char *s = va_arg(args, char *);
-			if (s == NULL)
-			{
-				printf("(nil)");
-			}
-			else
-			{
-				printf("%s", s);
-			}
-		}
+	
 		i++;
 	}
-	va_end(args);
+
 	printf("\n");
+
+	va_end(args);
 }
